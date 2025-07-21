7597c7164d7c08737c1fddb3ca80330aadb9b5ff
chalmani11rakshitha <chalmanirakshitha11@gmail.com>
2025-07-21 15:44:04 +0530

    variablesdemo

---
+++ variablesdemo.java
@@ -0,0 +1,19 @@
+public class variablesdemo {
+    //brand
+    //rating
+    //price
+    // datatype varname=value;
+    int somedatanew1;//declaration
+    int somedatanew2;//declaration
+    int price=350;
+    float rating=4.2f;
+    char textbrand='h';
+    public static void main(String[] args) {
+        int somedatanew1 =10; // initilization
+        int somedatanew2; // declaration
+        System.out.println(somedatanew1);
+        //system.out.println(somedatanew2);
+        
+    }
+
+}
